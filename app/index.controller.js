2854dbd1b390f58166b9604d8ef71eddf9be3314
Yeraldine Martínez <yerald02@hotmail.com>
2017-05-10 02:01:30 -0500

    Login completado

--- app/index.controller.js
+++ app/index.controller.js
@@ -3,10 +3,21 @@
 
   angular
     .module('minotaur')
-    .controller('MainController', MainController);
+    .controller('MainController', MainController)
+    .service('rootServices', rootServices);
 
+  
+  function handleError( response ) {
+      if ( ! angular.isObject( response.data ) || ! response.data.message ) {
+          return( $q.reject( "An unknown error occurred." ) );
+      }
+      return( $q.reject( response.data.message ) );
+  }
+  function handleSuccess( response ) {
+      return( response.data );
+  }
   /** @ngInject */
-  function MainController($translate) {
+  function MainController($translate, $scope, rootServices, $location) {
     var vm = this;
     // console.log('$translate',$translate); 
     vm.changeLanguage = function (langKey) { 
@@ -16,5 +27,73 @@
     };
     //vm.currentLanguage = $translate.proposedLanguage() || $translate.use();
     vm.changeLanguage('es');
+
+    $scope.fSessionCI = {};
+
+    $scope.isLoggedIn = false;
+    $scope.logOut = function() {
+      $scope.isLoggedIn = false;
+      $scope.captchaValido = false;
+    }
+
+    $scope.logIn = function() {
+      $scope.isLoggedIn = true;
+    };
+
+    $scope.btnLogoutToSystem = function () {
+      rootServices.sLogoutSessionCI().then(function () {
+        $scope.fSessionCI = {};
+        $scope.listaUnidadesNegocio = {};
+        $scope.listaModulos = {};
+        $scope.logOut();
+        $scope.goToUrl('/app/pages/login');
+      });
+    };
+
+    $scope.goToUrl = function ( path ) {
+      $location.path( path );
+    };
+
+    $scope.getValidateSession = function () {
+      rootServices.sGetSessionCI().then(function (response) {
+        //console.log(response);
+        if(response.flag == 1){
+          $scope.fSessionCI = response.datos;
+          $scope.logIn();
+          if( $location.path() == '/app/pages/login' ){
+            $scope.goToUrl('/');
+          }
+        }else{
+          $scope.fSessionCI = {};
+          $scope.logOut();
+          $scope.goToUrl('/app/pages/login');
+        }
+      });
+    }   
+  }
+
+  function rootServices($http, $q) {
+    return({
+        sLogoutSessionCI: sLogoutSessionCI,
+        sGetSessionCI: sGetSessionCI,
+    });
+    function sLogoutSessionCI(pDatos) { 
+      var datos = pDatos || {};
+      var request = $http({
+            method : "post",
+            url :  angular.patchURLCI + "Acceso/logoutSessionCI", 
+            data : datos          
+      });
+      return (request.then( handleSuccess,handleError ));
+    }
+    function sGetSessionCI(pDatos) { 
+      var datos = pDatos || {};
+      var request = $http({
+            method : "post",
+            url :  angular.patchURLCI + "Acceso/getSessionCI", 
+            data : datos          
+      });
+      return (request.then( handleSuccess,handleError ));
+    }
   }
 })();
--- app/pages/pages-login/pages-login.controller.js
+++ app/pages/pages-login/pages-login.controller.js
@@ -3,11 +3,70 @@
 
   angular
     .module('minotaur')
-    .controller('LoginController', LoginController);
+    .controller('LoginController', LoginController)
+    .service('loginServices', loginServices);
+
+    function handleError( response ) {
+      if ( ! angular.isObject( response.data ) || ! response.data.message ) {
+          return( $q.reject( "An unknown error occurred." ) );
+      }
+      return( $q.reject( response.data.message ) );
+  }
+  function handleSuccess( response ) {
+      return( response.data );
+  }
 
   /** @ngInject */
-  function LoginController() {
+  function LoginController($scope,loginServices) {
+  	$scope.getValidateSession();
+  	$scope.btnLoginToSystem = function () {
+      //console.log('esta en el Login');
+      if($scope.fLogin.usuario == null || $scope.fLogin.clave == null || $scope.fLogin.usuario == '' || $scope.fLogin.clave == ''){
+        $scope.fAlert = {};
+        $scope.fAlert.type= 'orange';
+        $scope.fAlert.msg= 'Debe completar los campos usuario y contraseña.';
+        $scope.fAlert.strStrong = 'Aviso.';
+        return;
+      }
 
+      loginServices.sLoginToSystem($scope.fLogin).then(function (response) { 
+        $scope.fAlert = {};
+        if( response.flag == 1 ){ // SE LOGEO CORRECTAMENTE 
+          $scope.fAlert.type= 'success';
+          $scope.fAlert.msg= response.message;
+          $scope.fAlert.strStrong = 'OK.';
+          $scope.getValidateSession();
+          $scope.logIn();
+          // $scope.getNotificaciones();
+        }else if( response.flag == 0 ){ // NO PUDO INICIAR SESION 
+          $scope.fAlert.type= 'danger';
+          $scope.fAlert.msg= response.message;
+          $scope.fAlert.strStrong = 'Error.';
+        }else if( response.flag == 2 ){  // CUENTA INACTIVA
+          $scope.fAlert.type= 'orange';
+          $scope.fAlert.msg= response.message;
+          $scope.fAlert.strStrong = 'Aviso.';
+          $scope.listaSedes = response.datos;
+        }
+        $scope.fAlert.flag = response.flag;
+        //$scope.fLogin = {};
+      });
+    }
+  }
+
+  function loginServices($http, $q) {
+    return({
+        sLoginToSystem: sLoginToSystem
+    });
+    function sLoginToSystem(pDatos) { 
+      var datos = pDatos || {};
+      var request = $http({
+            method : "post",
+            url :  angular.patchURLCI + "acceso/", 
+            data : datos          
+      });
+      return (request.then( handleSuccess,handleError ));
+    }
   }
 
 })();
