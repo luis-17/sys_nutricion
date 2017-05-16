50eca5dfc1732120cfce553725f6b5c816235b13
Yeraldine Martínez <yerald02@hotmail.com>
2017-05-15 21:25:15 -0500

    Citas

--- app/pages/citas/citas.controller.js
+++ app/pages/citas/citas.controller.js
@@ -1,127 +1,180 @@
-(function() {
-  'use strict';
+angular.module('minotaur')
+  .controller('CitasController', ['$scope', '$sce', '$filter', '$uibModal', '$window', '$http', '$log', '$timeout', 'CitasServices',
+    function($scope, $sce, $filter,  $uibModal, $window, $http, $log, $timeout, 
+      CitasServices,
+    ) {
+      var vm = this;
 
-  angular
-    .module('minotaur')
-    .controller('CitasController', CitasController);
+      var date = new Date();
+      var d = date.getDate();
+      var m = date.getMonth();
+      var y = date.getFullYear();
 
-  /** @ngInject */
-  function CitasController($scope) {
-    var vm = this;
+      vm.listarCitas = function(){
+        CitasServices.sListarCita().then(function (rpta) {
+          angular.forEach(rpta.datos, function(row, key) {
+            row.start = new Date(row.start);
+          });
+          //vm.events = rpta.datos;  
 
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
+          vm.events = [
+            {title: 'All Day Event',start: new Date(y, m, 1), className: ['b-l b-2x b-greensea']},
+            {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2), className: ['bg-dutch']},
+            {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false, className: ['b-l b-2x b-primary']},
+            {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false, className: ['b-l b-2x b-primary']},
+            {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false, className: ['b-l b-2x b-default']},
+            {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/', className: ['b-l b-2x b-hotpink']},
+            {title: 'Make cupcakes', start: new Date(y, m, 2), className: ['b-l b-2x b-info'], location:'Bratislava', info:'The best in whole world.'},
+            {title: 'Call wife', start: new Date(y, m, 6),end: new Date(y, m, 7), className: ['b-l b-2x b-red'], location:'Piestany', info:'And say her hello.'}
+          ];
 
-    /* event source that pulls from google.com */
-    vm.eventSource = {
-      url: 'http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic',
-      className: 'gcal-event',           // an option!
-      currentTimezone: 'America/Chicago' // an option!
-    };
+          vm.eventSources = [vm.events];
+          console.log(vm.eventSources);
+          angular.element('.calendar').fullCalendar('refetchEvents');
+        });
+      };
+      //vm.listarCitas();
+      
 
-    /* event source that contains custom events on the scope */
-    vm.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1), className: ['b-l b-2x b-greensea']},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2), className: ['bg-dutch']},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false, className: ['b-l b-2x b-primary']},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false, className: ['b-l b-2x b-primary']},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false, className: ['b-l b-2x b-default']},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/', className: ['b-l b-2x b-hotpink']},
-      {title: 'Make cupcakes', start: new Date(y, m, 2), className: ['b-l b-2x b-info'], location:'Bratislava', info:'The best in whole world.'},
-      {title: 'Call wife', start: new Date(y, m, 6),end: new Date(y, m, 7), className: ['b-l b-2x b-red'], location:'Piestany', info:'And say her hello.'}
-    ];
+      /* event source that contains custom events on the scope */
+      vm.events = [
+        {title: 'All Day Event',start: new Date(y, m, 1), className: ['b-l b-2x b-greensea']},
+        {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2), className: ['bg-dutch']},
+        {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false, className: ['b-l b-2x b-primary']},
+        {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false, className: ['b-l b-2x b-primary']},
+        {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false, className: ['b-l b-2x b-default']},
+        {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/', className: ['b-l b-2x b-hotpink']},
+        {title: 'Make cupcakes', start: new Date(y, m, 2), className: ['b-l b-2x b-info'], location:'Bratislava', info:'The best in whole world.'},
+        {title: 'Call wife', start: new Date(y, m, 6),end: new Date(y, m, 7), className: ['b-l b-2x b-red'], location:'Piestany', info:'And say her hello.'}
+      ];
+      vm.eventSources = [vm.events];
 
-    /* alert on dayClick */
-    vm.precision = 400;
-    vm.lastClickTime = 0;
-    vm.doubleClick = function(date){
-      var time = new Date().getTime();
-      if(time - vm.lastClickTime <= vm.precision){
+      /* alert on dayClick */
+      vm.precision = 400;
+      vm.lastClickTime = 0;
+      vm.click = function(start){
+        var time = new Date().getTime();
+        console.log('start',start);      
         vm.events.push({
           title: 'New Event',
-          start: date._d,
+          start: start,
           className: ['b-l b-2x b-info']
         });
+        vm.eventSources = [vm.events];        
+      };
+
+      /* alert on Drop */
+      vm.alertOnDrop = function(event, delta){
+        vm.alertMessage = ('Event Droped to make dayDelta ' + delta);
+      };
+
+      /* alert on Resize */
+      vm.alertOnResize = function(event, delta){
+        vm.alertMessage = ('Event Resized to make dayDelta ' + delta);
+      };
+
+      vm.overlay = angular.element('.fc-overlay');
+
+      vm.tooltipOnMouseOver = function( event, jsEvent){
+        vm.event = event;
+        vm.overlay.removeClass('left right');
+        var wrap = angular.element(jsEvent.target).closest('.fc-event');
+        var cal = wrap.closest('.calendar');
+        var left = wrap.offset().left - cal.offset().left;
+        var right = cal.width() - (wrap.offset().left - cal.offset().left + wrap.width());
+        if( right > vm.overlay.width() ) {
+          vm.overlay.addClass('left');
+        } else if ( left > vm.overlay.width() ) {
+          vm.overlay.addClass('right');
+        }
+        if (wrap.find('.fc-overlay').length === 0) {
+          wrap.append( vm.overlay );
+        }
+      };
+
+      vm.alertOnClick = function(event){
+        console.log('event',event);
       }
-      vm.lastClickTime = time;
-    };
 
-    /* alert on Drop */
-    vm.alertOnDrop = function(event, delta){
-      vm.alertMessage = ('Event Droped to make dayDelta ' + delta);
-    };
+      /* config object */
+      vm.uiConfig = {
+        calendar:{
+          height: 450,
+          contentHeight: 510,
+          editable: true,
+          dayNames: ["Domingo", "Lunes ", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"],
+          dayNamesShort : ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
+          header:{
+            left: 'prev',
+            center: 'title',
+            right: 'next',          
+          },          
+          dayClick: vm.click,
+          eventDrop: vm.alertOnDrop,
+          eventResize: vm.alertOnResize,          
+          //eventMouseover: vm.tooltipOnMouseOver,
+          eventClick: vm.alertOnClick,
+        }
+      };    
 
-    /* alert on Resize */
-    vm.alertOnResize = function(event, delta){
-      vm.alertMessage = ('Event Resized to make dayDelta ' + delta);
-    };
+      /* add custom event*/
+      vm.addEvent = function() {
+        vm.events.push({
+          title: 'New Event',
+          start: new Date(y, m, d),
+          className: ['b-l b-2x b-info']
+        });
+      };
 
-    vm.overlay = angular.element('.fc-overlay');
+      /* remove event */
+      vm.remove = function(index) {
+        vm.events.splice(index,1);
+      };
 
-    vm.tooltipOnMouseOver = function( event, jsEvent){
-      vm.event = event;
-      vm.overlay.removeClass('left right');
-      var wrap = angular.element(jsEvent.target).closest('.fc-event');
-      var cal = wrap.closest('.calendar');
-      var left = wrap.offset().left - cal.offset().left;
-      var right = cal.width() - (wrap.offset().left - cal.offset().left + wrap.width());
-      if( right > vm.overlay.width() ) {
-        vm.overlay.addClass('left');
-      } else if ( left > vm.overlay.width() ) {
-        vm.overlay.addClass('right');
+      /* Change View */
+      vm.changeView = function(view,calendar) {
+        /*console.log('entro aquii...');*/
+        angular.element('.calendar').fullCalendar('changeView', view);
+      };
+
+      vm.today = function() {
+        /*console.log('entro aquii...');*/
+        angular.element('.calendar').fullCalendar('changeView', 'agendaDay');
+        angular.element('.calendar').fullCalendar('today');
+      };      
+
+      vm.btnNuevaCita = function(){
+        console.log('btnNuevaCita');
       }
-      if (wrap.find('.fc-overlay').length === 0) {
-        wrap.append( vm.overlay );
-      }
-    };
 
-    /* config object */
-    vm.uiConfig = {
-      calendar:{
-        height: 450,
-        contentHeight: 510,
-        editable: true,
-        header:{
-          left: 'prev',
-          center: 'title',
-          right: 'next'
-        },
-        dayClick: vm.doubleClick,
-        eventDrop: vm.alertOnDrop,
-        eventResize: vm.alertOnResize,
-        eventMouseover: vm.tooltipOnMouseOver
-      }
-    };
+  }])
+  .service("CitasServices",function($http, $q) {
+    return({
+        sListarCita: sListarCita,
+        sRegistrar: sRegistrar,
 
-    /* add custom event*/
-    $scope.addEvent = function() {
-      vm.events.push({
-        title: 'New Event',
-        start: new Date(y, m, d),
-        className: ['b-l b-2x b-info']
+    });
+
+    function sListarCita(datos) {
+      var request = $http({
+            method : "post",
+            url : angular.patchURLCI+"Cita/listar_citas",
+            data : datos
       });
-    };
+      return (request.then(handleSuccess,handleError));
+      // return (request.then(function(response){
+      //   return( response.data );
 
-    /* remove event */
-    $scope.remove = function(index) {
-      vm.events.splice(index,1);
-    };
+      // }));
+    }
 
-    /* Change View */
-    $scope.changeView = function(view,calendar) {
-      console.log('entro aquii...');
-      angular.element('.calendar').fullCalendar('changeView', view);
-    };
+    function sRegistrar(datos) {
+      var request = $http({
+            method : "post",
+            url : angular.patchURLCI+"Paciente/registrar",
+            data : datos
+      });
+      return (request.then(handleSuccess,handleError));
+    }
 
-    $scope.today = function() {
-      angular.element('.calendar').fullCalendar('today');
-    };
-
-    /* event sources array*/
-    vm.eventSources = [vm.events];
-
-  }
-
-})();
+  });
---
+++ application/controllers/Cita.php
@@ -0,0 +1,71 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cita extends CI_Controller {
+	public function __construct(){
+        parent::__construct();
+        // Se le asigna a la informacion a la variable $sessionVP.
+        // $this->sessionVP = @$this->session->userdata('sess_vp_'.substr(base_url(),-8,7));
+        $this->load->helper(array('fechas_helper'));
+        $this->load->model(array('model_cita'));
+    }
+
+    public function listar_citas(){
+		$allInputs = json_decode(trim($this->input->raw_input_stream),true);
+		//$paramPaginate = $allInputs['paginate'];
+		$lista = $this->model_cita->m_cargar_citas();
+		// $totalRows = $this->model_paciente->m_count_pacientes($paramPaginate);
+		$arrListado = array();		
+		foreach ($lista as $row) {
+			if(empty($row['idatencion'])){
+				$className = array('bg-info');
+			}else{
+				$className = array('bg-success');
+			}
+			array_push($arrListado,
+				array(
+					'id' => $row['idcita'],
+					'hora_desde' => $row['hora_desde'],
+					'hora_hasta' => $row['hora_hasta'],
+					'estado_ci' => $row['estado_ci'],
+					'fecha' => $row['fecha'],
+					'cliente' => array(
+							'idcliente' => $row['idcliente'],
+							'cod_historia_clinica' => $row['cod_historia_clinica'],
+							'nombre' => $row['nombre'],
+							'apellidos' => $row['apellidos'],
+							'sexo' => $row['sexo'],
+						),
+					'profesional' => array(
+							'idprofesional' => $row['idprofesional'],
+							'profesional' => $row['profesional'],
+						),
+					'ubicacion' => array(
+							'idubicacion' => $row['idubicacion'],
+							'descripcion_ub' => $row['descripcion_ub'],
+						),
+					'atencion' => array(
+							'idatencion' => $row['idatencion'],
+							'fecha_atencion' => $row['fecha_atencion'],
+							'diagnostico_notas' => $row['diagnostico_notas']
+						),
+					'className' => $className,
+					'start' => $row['fecha'] .' '. $row['hora_desde'],
+					'title' => darFormatoHora($row['hora_desde']). ' - ' . darFormatoHora($row['hora_hasta']),
+					'allDay' => FALSE,
+				)
+			);
+		}
+
+    	$arrData['datos'] = $arrListado;
+    	$arrData['message'] = '';
+    	$arrData['flag'] = 1;
+		if(empty($lista)){
+			$arrData['flag'] = 0;
+		}
+		$this->output
+		    ->set_content_type('application/json')
+		    ->set_output(json_encode($arrData));
+	}
+
+}
