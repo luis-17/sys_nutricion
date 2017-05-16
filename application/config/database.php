c7575ccc80b8ebb2950b1781bc215b6f2edee626
Yeraldine Martínez <yerald02@hotmail.com>
2017-05-15 21:25:48 -0500

    db citas

--- application/config/database.php
+++ application/config/database.php
@@ -73,6 +73,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+/*$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => '138.197.29.60',
+	'username' => 'invitado',
+	'password' => 'conehost@',
+	'database' => 'sys_nutricion',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);*/
+
+
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => '138.197.29.60',
@@ -94,3 +117,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
