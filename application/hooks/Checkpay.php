1a2b2f29c1f1c5903438cf7ccab7aa7a4a9ecc20
Luis Ricardo <luis-ls17@hotmail.com>
2018-02-01 11:10:54 -0500

    se quito libreria mpdf y otras librerias js

--- application/hooks/Checkpay.php
+++ application/hooks/Checkpay.php
@@ -7,11 +7,13 @@ class Checkpay {
 
   function pay_check() { 
     $fCheck = $this->CI->model_usuario->m_verificar_usuario_check_pago();
-    if( $fCheck['mostrar_info_cobro'] == 3 ){
-      $arrData['flag'] = 'pay_expired';
-      $arrData['message'] = '<span class="text-danger">Tiene deuda vencida por servicios de infraestructura de servidor en la nube y mantenimiento del sistema informático.</span> <br /><br /> <b>CONTACTO </b>:  <br /> Luis Ricardo Luna Soto <br /> Freelancer - Desarrollador Web y Móvil <br /> CEL: 992566985 <br /> E-MAIL: luisls1717@gmail.com';
-      echo json_encode($arrData);
-      exit;
+    if($this->CI->uri->uri_string != 'acceso' && $this->CI->uri->uri_string != 'Acceso/getSessionCI' ){ 
+      if( $fCheck['mostrar_info_cobro'] == 3 ){
+        $arrData['flag'] = 'pay_expired';
+        $arrData['message'] = '<span class="text-danger">Tiene deuda vencida por servicios de infraestructura de servidor en la nube y mantenimiento del sistema informático.</span> <br /><br /> <b>CONTACTO </b>:  <br /> Luis Ricardo Luna Soto <br /> Freelancer - Desarrollador Web y Móvil <br /> CEL: 992566985 <br /> E-MAIL: luisls1717@gmail.com';
+        echo json_encode($arrData);
+        exit;
+      }
     }
   } 
 }
